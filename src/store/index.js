e7e532ae07344f856ddcf8decaf92213341dc6c4
tegArt <seoweb@inbox.ru>
2020-10-16 19:49:19 +0300

    vuex

--- src/store/index.js
+++ src/store/index.js
@@ -1,14 +1,19 @@
 import Vue from 'vue';
 import Vuex from 'vuex';
+import Axios from 'axios';
 
 Vue.use(Vuex);
 
 export default new Vuex.Store({
   state: {
+    isLoading: false,
     filter: {},
     productsByCategories: {},
   },
   mutations: {
+    setIsLoading: (state, payload) => {
+      state.isLoading = payload;
+    },
     setFilter: (state, payload) => {
       state.filter = payload;
     },
@@ -18,7 +23,9 @@ export default new Vuex.Store({
   },
   actions: {
     getProductsByCategories: context => {
-      this.$axios
+      context.commit('setIsLoading', true);
+
+      Axios
         .get('http://localhost:8081/products', {
           params: {
             filter: context.filter,
@@ -27,6 +34,8 @@ export default new Vuex.Store({
         .then(response => {
           let products = response.data.products;
           let productsByCategories = {};
+
+          console.log(products)
 
           products.forEach(product => {
             if (!Object.prototype.hasOwnProperty.call(productsByCategories, product.category)) {
@@ -37,9 +46,11 @@ export default new Vuex.Store({
           });
 
           context.commit('setProductsByCategories', productsByCategories);
+          context.commit('setIsLoading', false);
         })
         .catch(error => {
           console.log(error);
+          context.commit('setIsLoading', false);
         });
     }
   },
